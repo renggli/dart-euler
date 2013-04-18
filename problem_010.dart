6274ebba57d8743d095494b78e896c6d54ce580d
Lukas Renggli <renggli@gmail.com>
2013-04-18 19:52:08 +0200

    9, 10

---
+++ problem_010.dart
@@ -0,0 +1,15 @@
+/**
+ * Problem 10: Summation of primes
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+library problem_010;
+
+import 'problem_003.dart' as problem_003;
+
+void main() {
+  var max = 2000000;
+  print(problem_003.primesUpTo(max).reduce((a, b) => a + b)); // 142913828922
+}
