8b053e420a1f0690289355a5f336001c6859942f
Lukas Renggli <renggli@gmail.com>
2013-04-21 12:38:50 +0200

    20

---
+++ problem_020.dart
@@ -0,0 +1,20 @@
+/**
+ * Problem 20: Factorial digit sum
+ *
+ * n! means n  (n  1)  ...  3  2  1
+ *
+ * For example, 10! = 10  9  ...  3  2  1 = 3628800,
+ * and the sum of the digits in the number 10!
+ * is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+ *
+ * Find the sum of the digits in the number 100!
+ */
+library problem_020;
+
+import 'problem_015.dart';
+
+void main() {
+  print(factorial(100).toString().codeUnits
+      .map((each) => each - '0'.codeUnits.first)
+      .reduce((a, b) => a + b)); // 648
+}
