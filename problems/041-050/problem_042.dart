d683c174a90de2953eeed12c65e9b58488e81a1d
Lukas Renggli <renggli@gmail.com>
2013-05-25 13:58:00 +0100

    42 (partial)

--- problems/041-050/problem_042.dart
+++ problems/041-050/problem_042.dart
@@ -1,5 +1,21 @@
 /**
- * Problem 42:
+ * Problem 42: Coded triangle numbers
+ *
+ * The n-th term of te sequence of triangle numbers is given by
+ *
+ *    t_n = 0.5 * n * (n + 1)
+ *
+ * so the first thn traingle numbers are:
+ *
+ *    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+ *
+ * By converting each letter in a word to a number corresponding to its
+ * alphabetical position and adding these values we form a word value. For
+ * example, the word value for SKY is 19 + 11 + 25 = 55 = t_10. If the word
+ * value is a triangle number we shall call the word a triangle word.
+ *
+ * Using words.txt (right click and save), a 16K text file containing nearly
+ * 2000 common english words, how many are triangle words.
  */
 library problem_042;
 
@@ -9,6 +25,18 @@ import 'package:more/int_math.dart';
 import 'package:more/iterable.dart';
 import 'package:more/range.dart';
 
-void main() {
+final traingleNumbers = range(1000)
+    .map((value) => value * (value + 1) ~/ 2)
+    .toSet();
 
+final baseOffset = 'A'.codeUnitAt(0) - 1;
+
+int wordValue(String word) {
+  return word.toUpperCase().codeUnits
+      .map((each) => each - baseOffset)
+      .reduce((a, b) => a + b);
+}
+
+void main() {
+  print(wordValue('sky'));
 }
