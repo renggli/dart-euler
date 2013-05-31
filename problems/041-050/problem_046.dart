8a39803e7a6b6a86a84197c8de2db93b3ad054ab
Lukas Renggli <renggli@gmail.com>
2013-05-31 22:54:07 +0200

    46

--- problems/041-050/problem_046.dart
+++ problems/041-050/problem_046.dart
@@ -1,5 +1,20 @@
 /**
- * Problem 46:
+ * Problem 46: Goldbach's other conjecture
+ *
+ * It was proposed by Christian Goldbach that every odd composite number can
+ * be written as the sum of a prime and twice a square.
+ *
+ * 9 = 7 + 2 * 1^2
+ * 15 = 7 + 2 * 2^2
+ * 21 = 3 + 2 * 3^2
+ * 25 = 7 + 2 * 3^2
+ * 27 = 19 + 2 * 2^2
+ * 33 = 31 + 2 * 1^2
+ *
+ * It turns out that the conjecture was false.
+ *
+ * What is the smallest odd composite that cannot be written as the sum of a
+ * prime and twice a square?
  */
 library problem_046;
 
@@ -9,6 +24,29 @@ import 'package:more/int_math.dart';
 import 'package:more/iterable.dart';
 import 'package:more/range.dart';
 
+final primes = primesUpTo(1000000);
+final prime_set = primes.toSet();
+
+bool verifySquare(int n) {
+  if (n.isOdd) {
+    return false;
+  }
+  var m = n ~/ 2;
+  var s = sqrt(m).truncate();
+  return s * s == m;
+}
+
+bool verify(int n) {
+  return primes
+      .takeWhile((p) => p <= n - 2)
+      .any((p) => verifySquare(n - p));
+}
+
 void main() {
-  assert(false);
+  for (var n = 3; ; n += 2) {
+    if (!prime_set.contains(n) && !verify(n)) {
+      assert(n == 5777);
+      return;
+    }
+  }
 }
