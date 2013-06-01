c854ed442bc30ff944c92ab2fdabf6618038679d
Lukas Renggli <renggli@gmail.com>
2013-06-01 23:19:34 +0200

    48

--- problems/041-050/problem_048.dart
+++ problems/041-050/problem_048.dart
@@ -1,14 +1,20 @@
 /**
- * Problem 48:
+ * Problem 48: Self powers
+ *
+ * The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+ *
+ * Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
  */
 library problem_048;
 
-import 'dart:math';
-import 'package:more/bit_set.dart';
 import 'package:more/int_math.dart';
-import 'package:more/iterable.dart';
 import 'package:more/range.dart';
 
+final max = 1000;
+
 void main() {
-  assert(false);
+  var sum = range(1, max + 1)
+      .map((i) => pow(i, i))
+      .reduce((a, b) => a + b);
+  assert(sum % pow(10, 10) == 9110846700);
 }
