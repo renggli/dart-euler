fdaa2990427d0486387369dd476dd1f6686faeb2
Lukas Renggli <renggli@gmail.com>
2013-06-09 14:37:36 +0200

    51-60 (with task)

--- problems/051-060/problem_052.dart
+++ problems/051-060/problem_052.dart
@@ -1,7 +1,13 @@
 /**
- * Problem XX:
+ * Problem 52: Permuted multiples
+ *
+ * It can be seen that the number, 125874, and its double, 251748, contain
+ * exactly the same digits, but in a different order.
+ *
+ * Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+ * contain the same digits.
  */
-library problem_0XX;
+library problem_052;
 
 import 'dart:math';
 import 'package:more/bit_set.dart';
