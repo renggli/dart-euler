3eeab5a2bff312bac1de1ff920b7b072ad2d04c8
Lukas Renggli <renggli@gmail.com>
2013-07-10 00:08:56 +0200

    60

--- problems/051-060/problem_060.dart
+++ problems/051-060/problem_060.dart
@@ -12,13 +12,48 @@
  */
 library problem_060;
 
-import 'dart:math';
-import 'package:more/bit_set.dart';
-import 'package:more/ordering.dart';
 import 'package:more/int_math.dart';
-import 'package:more/iterable.dart';
-import 'package:more/range.dart';
+
+final primes = primesUpTo(10000);
+final cache = new Map();
+
+bool allPrime(List<List<num>> pairs) {
+  for (var pair in pairs) {
+    var s1 = pair[0].toString(), s2 = pair[1].toString();
+    if (!isProbablyPrime(int.parse(s1 + s2)) ||
+        !isProbablyPrime(int.parse(s2 + s1))) {
+      return false;
+    }
+  }
+  return true;
+}
 
 void main() {
+  var result = 0;
+  for (var pi1 = 0; pi1 < primes.length; pi1++) {
+    var p1 = primes[pi1];
+    for (var pi2 = pi1 + 1; pi2 < primes.length; pi2++) {
+      var p2 = primes[pi2];
+      if (allPrime([[p1, p2]])) {
+        for (var pi3 = pi2 + 1; pi3 < primes.length; pi3++) {
+          var p3 = primes[pi3];
+          if (allPrime([[p1, p3], [p2, p3]])) {
+            for (var pi4 = pi3 + 1; pi4 < primes.length; pi4++) {
+              var p4 = primes[pi4];
+              if (allPrime([[p1, p4], [p2, p4], [p3, p4]])) {
+                for (var pi5 = pi4 + 1; pi5 < primes.length; pi5++) {
+                  var p5 = primes[pi5];
+                  if (allPrime([[p1, p5], [p2, p5], [p3, p5], [p4, p5]])) {
+                    assert(p1 + p2 + p3 + p4 + p5 == 26033);
+                    return;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
   assert(false);
 }
