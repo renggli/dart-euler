e70a4438ad2a56efc3d74a0d7f1d063f3257fd46
Lukas Renggli <renggli@gmail.com>
2013-09-08 22:21:20 +0200

    63

--- example/061-070/problem_063.dart
+++ example/061-070/problem_063.dart
@@ -4,17 +4,24 @@
  * The 5-digit number, 16807=7^5, is also a fifth power. Similarly, the 9-digit number,
  * 134217728=8^9, is a ninth power.
  *
- * How many n-digit positive integers exist which are also an nth power?
+ * How many n-digit positive integers exist which are also an n-th power?
  */
 library problem_063;
 
-import 'dart:math';
-import 'package:more/bit_set.dart';
-import 'package:more/ordering.dart';
 import 'package:more/int_math.dart';
 import 'package:more/iterable.dart';
-import 'package:more/range.dart';
 
 void main() {
-  assert(false);
+  var count = 0;
+  for (var a = 1; a < 25; a++) {
+    for (var b = 1; b < 25; b++) {
+      var len = digits(pow(a, b)).length;
+      if (len == b) {
+        count++;
+      } else if (len > b) {
+        break;
+      }
+    }
+  }
+  assert(count == 49);
 }
