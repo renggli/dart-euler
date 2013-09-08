f22109ed625bb20942bfad115cf227342235c7ab
Lukas Renggli <renggli@gmail.com>
2013-09-08 19:42:11 +0200

    67

--- example/061-070/problem_061.dart
+++ example/061-070/problem_061.dart
@@ -27,13 +27,51 @@
  */
 library problem_061;
 
-import 'dart:math';
-import 'package:more/bit_set.dart';
-import 'package:more/ordering.dart';
-import 'package:more/int_math.dart';
-import 'package:more/iterable.dart';
-import 'package:more/range.dart';
+List<int> generate(int func(int)) {
+  var result = new List();
+  for (var n = 1, v = 1; v < 10000; n++, v = func(n)) {
+    if (v > 999) {
+      result.add(v);
+    }
+  }
+  return new List.from(result, growable: false);
+}
+
+final List<List<int>> types = [
+  generate((n) => n * (n + 1) ~/ 2),
+  generate((n) => n * n),
+  generate((n) => n * (3 * n - 1) ~/ 2),
+  generate((n) => n * (2 * n - 1)),
+  generate((n) => n * (5 * n - 3) ~/ 2),
+  generate((n) => n * (3 * n - 2))];
+
+List<List<int>> findChains(List<int> indexes, int prefix) {
+  var chains = new List();
+  if (indexes.isEmpty) {
+    chains.add([]);
+  } else {
+    for (var index in indexes) {
+      var new_indexes = indexes
+          .where((value) => value != index)
+          .toList();
+      for (var number in types[index]) {
+        if (prefix == -1 || number ~/ 100 == prefix) {
+          for (var chain in findChains(new_indexes, number % 100)) {
+            chains.add(chain..insert(0, number));
+          }
+        }
+      }
+    }
+  }
+  return chains;
+}
 
 void main() {
+  for (var chain in findChains([0, 1, 2, 3, 4, 5], -1)) {
+    if (chain.first ~/ 100 == chain.last % 100) {
+      assert(chain.reduce((a, b) => a + b) == 28684);
+      return;
+    }
+  }
   assert(false);
 }
