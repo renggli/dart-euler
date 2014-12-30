db31fa49d384808249f62172857995ff6a03f3ae
Lukas Renggli <renggli@gmail.com>
2014-12-30 14:52:37 +0100

    Fix the error reporting when running the tests

--- test/all_test.dart
+++ test/all_test.dart
@@ -6,6 +6,10 @@ import 'package:unittest/unittest.dart';
 void main() {
   allProblemsDo((problem, executor) {
     test('Problem $problem', () {
+      var result = executor();
+      if (result.exitCode != 0) {
+        logMessage(result.stderr);
+      }
       expect(executor().exitCode, 0, reason: 'Exit code');
     });
   }, arguments: ['--checked']);
