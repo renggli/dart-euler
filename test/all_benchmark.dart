8936878d5ef009aa4df473d22e2ebc2c90fcb751
Lukas Renggli <renggli@gmail.com>
2014-12-30 14:52:24 +0100

    Add benchmarking code

---
+++ test/all_benchmark.dart
@@ -0,0 +1,18 @@
+library all_benchmark;
+
+import 'package:euler/euler.dart';
+
+void main() {
+  print('<?xml version="1.0"?>');
+  print('<benchmarks>');
+  allProblemsDo((problem, executor) {
+    var watch = new Stopwatch();
+    watch.start();
+    var result = executor();
+    var elapsed = watch.elapsedMilliseconds;
+    if (result.exitCode == 0) {
+      print('  <benchmark name="$problem">${elapsed}</benchmark>');
+    }
+  });
+  print('</benchmarks>');
+}
