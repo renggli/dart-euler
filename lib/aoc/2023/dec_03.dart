453d4d9a6360fa397095c3c8b967551c759fdde7
Lukas Renggli <renggli@gmail.com>
2023-12-03 07:57:29 +0100

    AoC: Dec 3 (cleanup)

--- lib/aoc/2023/dec_03.dart
+++ lib/aoc/2023/dec_03.dart
@@ -4,15 +4,13 @@ import 'dart:math';
 import 'package:data/data.dart';
 import 'package:more/more.dart';
 
-final matrix = File('lib/aoc/2023/dec_03.txt')
-    .readAsLinesSync()
-    .map((line) => line.toList())
-    .toList();
+final data = File('lib/aoc/2023/dec_03.txt').readAsLinesSync().toList();
 
-int part1() {
-  var sum = 0;
-  for (var i = 0; i < matrix.length; i++) {
-    final row = matrix[i];
+// Mapping from position-symbol to neighbouring numbers.
+final neighbours = () {
+  final result = ListMultimap<(int, int, {String char}), int>();
+  for (var i = 0; i < data.length; i++) {
+    final row = data[i];
     for (var j1 = 0; j1 < row.length;) {
       var j2 = j1;
       while (j2 < row.length && row[j2].startsWith(const CharMatcher.digit())) {
@@ -21,42 +19,12 @@ int part1() {
       if (j1 == j2) {
         j1++;
       } else {
-        neighbourSearch:
-        for (var x = max(i - 1, 0); x < min(i + 2, matrix.length); x++) {
+        for (var x = max(i - 1, 0); x < min(i + 2, data.length); x++) {
           for (var y = max(j1 - 1, 0); y < min(j2 + 1, row.length); y++) {
-            if (matrix[x][y] != '.' &&
-                matrix[x][y].startsWith(const CharMatcher.punctuation())) {
-              sum += int.parse(row.sublist(j1, j2).join(''));
-              break neighbourSearch;
-            }
-          }
-        }
-        j1 += j2 - j1;
-      }
-    }
-  }
-  return sum;
-}
-
-int part2() {
-  final neighbours = ListMultimap<(int, int, String), int>();
-  for (var i = 0; i < matrix.length; i++) {
-    final row = matrix[i];
-    for (var j1 = 0; j1 < row.length;) {
-      var j2 = j1;
-      while (j2 < row.length && row[j2].startsWith(const CharMatcher.digit())) {
-        j2++;
-      }
-      if (j1 == j2) {
-        j1++;
-      } else {
-        for (var x = max(i - 1, 0); x < min(i + 2, matrix.length); x++) {
-          for (var y = max(j1 - 1, 0); y < min(j2 + 1, row.length); y++) {
-            final char = matrix[x][y];
+            final char = data[x][y];
             if (char != '.' &&
                 char.startsWith(const CharMatcher.punctuation())) {
-              neighbours
-                  .add((x, y, char), int.parse(row.sublist(j1, j2).join('')));
+              result.add((x, y, char: char), int.parse(row.substring(j1, j2)));
             }
           }
         }
@@ -64,17 +32,16 @@ int part2() {
       }
     }
   }
-
-  var sum = 0;
-  for (var MapEntry(:key, :value) in neighbours.asMap().entries) {
-    if (key.$3 == '*' && value.length == 2) {
-      sum += value.product();
-    }
-  }
-  return sum;
-}
+  return result;
+}();
 
 void main() {
-  assert(part1() == 535235);
-  assert(part2() == 79844424);
+  assert(neighbours.values.sum() == 535235);
+  assert(neighbours
+          .asMap()
+          .entries
+          .where((entry) => entry.key.char == '*' && entry.values.length == 2)
+          .map((entry) => entry.values.product())
+          .sum() ==
+      79844424);
 }
