9eedb38c1a9f0114e715a5ea13712d4c0988ff18
Lukas Renggli <renggli@gmail.com>
2023-12-08 06:49:41 +0100

    AoC: Dec 8

---
+++ lib/aoc/2023/dec_08.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:more/more.dart';
+
+final data = File('lib/aoc/2023/dec_08.txt').readAsStringSync().split('\n\n');
+final instructions = data[0].split('');
+final network = data[1]
+    .split('\n')
+    .map((line) => line.split(RegExp(r'[ =(,)]+')))
+    .toMap<String, List<String>>(
+      key: (parts) => parts[0],
+      value: (parts) => parts.sublist(1, 3),
+    );
+
+int cycleLength(String start, bool Function(String) end) {
+  var count = 1;
+  var current = start;
+  for (final instruction in instructions.repeat()) {
+    current = network[current]![instruction == 'L' ? 0 : 1];
+    if (end(current)) break;
+    count++;
+  }
+  return count;
+}
+
+int problem1() => cycleLength('AAA', (each) => each == 'ZZZ');
+
+int problem2() => network.keys
+    .where((each) => each.endsWith('A'))
+    .map((each) => cycleLength(each, (each) => each.endsWith('Z')))
+    .reduce((a, b) => a.lcm(b));
+
+void main() {
+  assert(problem1() == 12169);
+  assert(problem2() == 12030780859469);
+}
