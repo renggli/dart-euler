8a7851e5252a60b18648400803557ef8e884551c
Lukas Renggli <renggli@gmail.com>
2024-12-01 08:46:03 +0100

    AoC: Dec 1, 2024

---
+++ lib/aoc/2024/dec_01.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'package:data/data.dart';
+import 'package:more/more.dart';
+
+final whitespace = RegExp(r'\s+');
+final input = File('lib/aoc/2024/dec_01.txt')
+    .readAsLinesSync()
+    .map((values) => values.split(whitespace).map(int.parse))
+    .zip()
+    .map((list) => list.toSortedList())
+    .toList();
+
+int problem1() =>
+    input.zip().map((values) => (values[0] - values[1]).abs()).sum();
+
+int problem2() {
+  final counts = input[1].toMultiset();
+  return input[0].map((each) => each * counts[each]).sum();
+}
+
+void main() {
+  assert(problem1() == 765748);
+  assert(problem2() == 27732508);
+}
