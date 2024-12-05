37b92d4b9d16e838f21bf3f569db18a091447318
Lukas Renggli <renggli@gmail.com>
2024-12-05 06:47:11 +0100

    AoC: Dec 5, 2024

---
+++ lib/aoc/2024/dec_05.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+import 'package:more/more.dart';
+
+final rulesAndUpdates =
+    File('lib/aoc/2024/dec_05.txt').readAsStringSync().trim().split('\n\n');
+final rules = SetMultimap.fromEntries(rulesAndUpdates.first
+    .split('\n')
+    .map((line) => line.split('|').map(int.parse).toList())
+    .map((pair) => MapEntry(pair.first, pair.last)));
+final updates = rulesAndUpdates.last
+    .split('\n')
+    .map((line) => line.split(',').map(int.parse).toList())
+    .toList();
+
+int comparator(int a, int b) => rules.containsKeyAndValue(a, b) ? -1 : 1;
+
+int problem1() => updates
+    .where(comparator.isStrictlyOrdered)
+    .map((update) => update[update.length ~/ 2])
+    .reduce((a, b) => a + b);
+
+int problem2() => updates
+    .whereNot(comparator.isStrictlyOrdered)
+    .map((update) => update.sorted(comparator))
+    .map((update) => update[update.length ~/ 2])
+    .reduce((a, b) => a + b);
+
+void main() {
+  assert(problem1() == 3608);
+  assert(problem2() == 4922);
+}
