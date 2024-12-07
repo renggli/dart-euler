146eaf5e1cfa1b2ecdea3cb8dc614a43c8ebada6
Lukas Renggli <renggli@gmail.com>
2024-12-07 08:35:04 +0100

    AoC: Dec 7, 2024

---
+++ lib/aoc/2024/dec_07.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:more/more.dart';
+
+final input = File('lib/aoc/2024/dec_07.txt').readAsLinesSync();
+final equations = input
+    .map((line) => line.split(RegExp('[: ]+')).map(int.parse))
+    .map((line) => (result: line.first, values: line.skip(1).toList()))
+    .toList();
+
+int add(int a, int b) => a + b;
+int mul(int a, int b) => a * b;
+int concat(int a, int b) {
+  var base = 10;
+  while (b >= base) {
+    base *= 10;
+  }
+  return a * base + b;
+}
+
+bool test(int expected, List<int> values, List<int Function(int, int)> ops) {
+  for (final operators in repeat(ops, count: values.length - 1).product()) {
+    var actual = values.first;
+    for (var o = 0; o < operators.length; o++) {
+      actual = operators[o](actual, values[o + 1]);
+    }
+    if (actual == expected) return true;
+  }
+  return false;
+}
+
+int run(List<int Function(int, int)> ops) => equations
+    .where((equation) => test(equation.result, equation.values, ops))
+    .fold(0, (result, equation) => result + equation.result);
+
+int problem1() => run([add, mul]);
+
+int problem2() => run([add, mul, concat]);
+
+void main() {
+  assert(problem1() == 28730327770375);
+  assert(problem2() == 424977609625985);
+}
