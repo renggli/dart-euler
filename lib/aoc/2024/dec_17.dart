3a785cf113102a48c4fc03741eb415bb628e0ecd
Lukas Renggli <renggli@gmail.com>
2024-12-17 13:04:27 +0100

    AoC: Dec 17, 2024

---
+++ lib/aoc/2024/dec_17.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
+
+import 'utils/machine.dart';
+
+final input = File('lib/aoc/2024/dec_17.txt').readAsStringSync();
+final values =
+    RegExp(r'\d+').allMatches(input).map((each) => each[0]!).map(int.parse);
+final registers = values.take(3).toList();
+final program = values.skip(3).toList();
+
+String problem1() => Machine(registers, program).run().join(',');
+
+List<int> run(int a) => Machine([a, ...registers.skip(1)], program).run();
+
+int problem2() {
+  var register = 0;
+  const equality = ListEquality<int>();
+  for (var start = program.length - 1; start >= 0; start--) {
+    var current = register << 3;
+    final expected = program.sublist(start);
+    while (true) {
+      final output = run(current);
+      if (equality.equals(output, expected)) {
+        register = current;
+        break;
+      }
+      current++;
+    }
+  }
+  assert(equality.equals(run(register), program));
+  return register;
+}
+
+void main() {
+  assert(problem1() == '2,3,6,2,1,6,1,2,1');
+  assert(problem2() == 90938893795561);
+}
---
+++ lib/aoc/2024/utils/machine.dart
@@ -0,0 +1,59 @@
+class Machine {
+  Machine(Iterable<int> registers, Iterable<int> program)
+      : registers = registers.toList(growable: false),
+        program = program.toList(growable: false) {
+    assert(registers.length == 3, 'Exactly 3 registers expected');
+  }
+
+  final List<int> registers;
+  final List<int> program;
+  final List<int> output = [];
+
+  int _ip = 0;
+
+  List<int> run() {
+    while (_ip < program.length) {
+      step();
+    }
+    return output;
+  }
+
+  void step() {
+    final opcode = program[_ip];
+    final operand = program[_ip + 1];
+    switch (opcode) {
+      case 0: // adv instruction
+        registers[registerA] = registers[registerA] >> _combo(operand);
+      case 1: // bxl instruction
+        registers[registerB] = registers[registerB] ^ operand;
+      case 2: // bst instruction
+        registers[registerB] = _combo(operand) & 7;
+      case 3: // jnz instruction
+        if (registers[registerA] != 0) {
+          _ip = operand;
+          return;
+        }
+      case 4: // bxc instruction
+        registers[registerB] = registers[registerB] ^ registers[registerC];
+      case 5: // out instruction
+        output.add(_combo(operand) & 7);
+      case 6: // bdv instruction
+        registers[registerB] = registers[registerA] >> _combo(operand);
+      case 7: // cdv instruction
+        registers[registerC] = registers[registerA] >> _combo(operand);
+      default:
+        throw AssertionError('Invalid op-code: $opcode');
+    }
+    _ip += 2;
+  }
+
+  int _combo(int operand) => operand <= 3
+      ? operand // literal
+      : operand <= 6
+          ? registers[operand - 4] // register
+          : throw AssertionError('Invalid operand: $operand');
+
+  static const registerA = 0;
+  static const registerB = 1;
+  static const registerC = 2;
+}
