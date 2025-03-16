8c8e2f9ff1f60a032b2083422121d2ca9d7a8898
Lukas Renggli <renggli@gmail.com>
2025-03-16 08:52:54 +0100

    Add day 8 and 9

--- lib/i18n/08.dart
+++ lib/i18n/08.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
-int run(String filename) {
-  final input = File(filename).readAsStringSync();
-  // TODO: solve the puzzle
-  return input.length;
+import 'package:more/more.dart';
+import 'package:string_normalizer/string_normalizer.dart';
+
+bool isValid(String password) {
+  final normal = password.normalize().toLowerCase();
+  return password.length.between(4, 12) &&
+      UnicodeCharMatcher.numberDecimalDigit().countIn(password) >= 1 &&
+      CharMatcher.pattern('aeiou').countIn(normal) >= 1 &&
+      (UnicodeCharMatcher.letter() & CharMatcher.pattern('^aeiou')).countIn(
+            normal,
+          ) >=
+          1 &&
+      normal.runes.length == normal.runes.toSet().length;
 }
 
+int run(String filename) => File(filename).readAsLinesSync().count(isValid);
+
 void main() {
-  print(run('lib/i18n/08-test.txt'));
-  print(run('lib/i18n/08-input.txt'));
+  assert(run('lib/i18n/08-test.txt') == 2);
+  assert(run('lib/i18n/08-input.txt') == 809);
 }
--- lib/i18n/09.dart
+++ lib/i18n/09.dart
@@ -1,12 +1,67 @@
 import 'dart:io';
 
-int run(String filename) {
-  final input = File(filename).readAsStringSync();
-  // TODO: solve the puzzle
-  return input.length;
+import 'package:more/collection.dart';
+
+const formats = {
+  'DMY': [31, 12, 99],
+  'MDY': [12, 31, 99],
+  'YMD': [99, 12, 31],
+  'YDM': [99, 31, 12],
+};
+final nineEleven = DateTime(2001, 9, 11);
+
+DateTime? createDate(String format, List<int> input) {
+  final year = 2000 + input[format.indexOf('Y')];
+  final month = input[format.indexOf('M')];
+  final day = input[format.indexOf('D')];
+  final result = DateTime(year, month, day);
+  return result.year == year && result.month == month && result.day == day
+      ? result
+      : null;
+}
+
+String deriveFormat(List<List<int>> dates) {
+  for (final MapEntry(key: name, value: format) in formats.entries) {
+    if (dates.every((date) {
+      for (var i = 0; i < 3; i++) {
+        if (date[i] > format[i]) {
+          return false;
+        }
+      }
+      return createDate(name, date) != null;
+    })) {
+      return name;
+    }
+  }
+  throw StateError('Unknown format for $dates');
+}
+
+String run(String filename) {
+  final nameToDates = ListMultimap<String, List<int>>();
+  for (final input in File(filename).readAsLinesSync()) {
+    final parts = input.takeTo(': ').split('-').map(int.parse).toList();
+    final names = input.skipTo(': ').split(', ').toList();
+    for (final name in names) {
+      nameToDates.add(name, parts);
+    }
+  }
+  final names = <String>[];
+  for (final name in nameToDates.keys) {
+    final format = deriveFormat(nameToDates[name]);
+    if (nameToDates[name].any(
+      (date) => createDate(format, date) == nineEleven,
+    )) {
+      names.add(name);
+    }
+  }
+  names.sort();
+  return names.join(' ');
 }
 
 void main() {
-  print(run('lib/i18n/09-test.txt'));
-  print(run('lib/i18n/09-input.txt'));
+  assert(run('lib/i18n/09-test.txt') == 'Margot Peter');
+  assert(
+    run('lib/i18n/09-input.txt') ==
+        'Amelia Amoura Hugo Jack Jakob Junior Mateo',
+  );
 }
