05649cfdc04df7f96c58def9a4d874b66aced718
Lukas Renggli <renggli@gmail.com>
2025-08-15 16:44:35 +0200

    Fix the broken puzzle.

--- lib/i18n/puzzle_12.dart
+++ lib/i18n/puzzle_12.dart
@@ -1,63 +1,63 @@
-import 'dart:io';
+// import 'dart:io';
+// import 'package:collection/collection.dart';
+// import 'package:intl4x/intl4x.dart';
+// import 'package:more/more.dart';
 
-import 'package:collection/collection.dart';
-import 'package:intl4x/intl4x.dart';
-import 'package:more/more.dart';
+// class Entry {
+//   factory Entry.fromString(String input) {
+//     final parts = input.split(': ');
+//     final names = parts[0].split(', ');
+//     return Entry(family: names[0], first: names[1], phone: int.parse(parts[1]));
+//   }
 
-class Entry {
-  factory Entry.fromString(String input) {
-    final parts = input.split(': ');
-    final names = parts[0].split(', ');
-    return Entry(family: names[0], first: names[1], phone: int.parse(parts[1]));
-  }
+//   Entry({required this.family, required this.first, required this.phone});
 
-  Entry({required this.family, required this.first, required this.phone});
+//   final String family;
+//   final String first;
+//   final int phone;
 
-  final String family;
-  final String first;
-  final int phone;
+//   @override
+//   String toString() => '$family, $first: ${phone.toString().padLeft(7, '0')}';
+// }
 
-  @override
-  String toString() => '$family, $first: ${phone.toString().padLeft(7, '0')}';
-}
+// List<Entry> sortedEntries(
+//   Iterable<Entry> entries, {
+//   required String language,
+//   required String Function(Entry) keyOf,
+// }) => entries.sortedByCompare(
+//   keyOf,
+//   Intl(locale: Locale(language: language)).collation().compare,
+// );
 
-List<Entry> sortedEntries(
-  Iterable<Entry> entries, {
-  required String language,
-  required String Function(Entry) keyOf,
-}) => entries.sortedByCompare(
-  keyOf,
-  Intl(locale: Locale(language: language)).collation().compare,
-);
-
-int run(String filename) {
-  final entries = File(filename).readAsLinesSync().map(Entry.fromString);
-  final english = sortedEntries(
-    entries,
-    language: 'en',
-    keyOf: (entry) => UnicodeCharMatcher.letter()
-        .retainFrom(entry.family.replaceAll('Æ', 'ae'))
-        .toLowerCase(),
-  );
-  final swedish = sortedEntries(
-    entries,
-    language: 'sv',
-    keyOf: (entry) => UnicodeCharMatcher.letter().retainFrom(
-      entry.family.replaceAll('Æ', 'Ä').replaceAll('Ø', 'Ö'),
-    ),
-  );
-  final dutch = sortedEntries(
-    entries,
-    language: 'nl',
-    keyOf: (entry) => entry.family.removePrefix(
-      entry.family.takeTo(UnicodeCharMatcher.letterUppercase()),
-    ),
-  );
-  final middle = entries.length ~/ 2;
-  return english[middle].phone * swedish[middle].phone * dutch[middle].phone;
-}
+// int run(String filename) {
+//   final entries = File(filename).readAsLinesSync().map(Entry.fromString);
+//   final english = sortedEntries(
+//     entries,
+//     language: 'en',
+//     keyOf: (entry) => UnicodeCharMatcher.letter()
+//         .retainFrom(entry.family.replaceAll('Æ', 'ae'))
+//         .toLowerCase(),
+//   );
+//   final swedish = sortedEntries(
+//     entries,
+//     language: 'sv',
+//     keyOf: (entry) => UnicodeCharMatcher.letter().retainFrom(
+//       entry.family.replaceAll('Æ', 'Ä').replaceAll('Ø', 'Ö'),
+//     ),
+//   );
+//   final dutch = sortedEntries(
+//     entries,
+//     language: 'nl',
+//     keyOf: (entry) => entry.family.removePrefix(
+//       entry.family.takeTo(UnicodeCharMatcher.letterUppercase()),
+//     ),
+//   );
+//   final middle = entries.length ~/ 2;
+//   return english[middle].phone * swedish[middle].phone * dutch[middle].phone;
+// }
 
 void main() {
-  assert(run('lib/i18n/puzzle_12_test.txt') == 1885816494308838);
-  assert(run('lib/i18n/puzzle_12_input.txt') == 5722005348122931);
+  // assert(run('lib/i18n/puzzle_12_test.txt') == 1885816494308838);
+  // assert(run('lib/i18n/puzzle_12_input.txt') == 5722005348122931);
+  assert(false, 'Add assertions for solved puzzle');
 }
